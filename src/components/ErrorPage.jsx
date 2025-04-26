f1e924549191e286ba4c3a278d5285cb47140305
kinya1738 <shirleen.njeru@student.moringaschool.com>
2025-04-25 21:13:00 -0700

    final commit

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+function ErrorPage() {
+  return (
+    <>
+      <NavBar />
+      <h1>Oops! Looks like something went wrong.</h1>
+    </>
+  );
+}
+
+export default ErrorPage;
